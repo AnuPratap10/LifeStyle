3f2c8b24a586f676d91b9aacddbe7a048f2213fd
AnuPratap10 <anupratapsingh49@gmail.com>
2023-03-06 23:44:00 +0530

    a

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://powerful-pink-fly.cyclic.app/"
-const BASE_URL = "http://localhost:8080/"
+const BASE_URL = "https://powerful-pink-fly.cyclic.app/"
+// const BASE_URL = "http://localhost:8080/"
 
 
 export default BASE_URL
